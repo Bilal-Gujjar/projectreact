fbe3aba39c61b8bbc03800a8b98f99591db4ce3f
Bilal Gujjar <bilaltahir515@gmail.com>
2020-06-04 01:28:03 +0500

    Std

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Page is created and deployed by Bilal Gurjjar
           <br/>Update by Ch.Bilal Gujjar
         </p>
       
