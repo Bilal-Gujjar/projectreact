b8d194b827e086d0fc49d441a9cf994adc304dec
Bilal-Gujjar <Bilaltahir515@gmail.com>
2020-06-04 08:09:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This ia a single page Web App
+        <p>This is a single page Web App
           <br/>
            created and deployed by Ch.Bilal Gujjar and team!
         </p>
